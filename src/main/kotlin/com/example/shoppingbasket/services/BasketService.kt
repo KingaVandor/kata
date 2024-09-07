5df5d743c4ecf4e3d0b01744ea051cfba467fa03
kinga.vandor <kingavandor@yahoo.com>
2024-09-07 17:16:32 +0100

    created return object for update request calls, that lists latest state of basket

--- src/main/kotlin/com/example/shoppingbasket/services/BasketService.kt
+++ src/main/kotlin/com/example/shoppingbasket/services/BasketService.kt
@@ -6,26 +6,27 @@ import org.springframework.stereotype.Component
 class BasketService  {
     val basketMap = mutableMapOf<Long, MutableMap<Int, Int>>()
 
-    fun addItemToBasket(request: BasketUpdateRequest) {
+    fun addItemToBasket(request: BasketUpdateRequest): List<Item> {
         val productCode = request.product.productCode
 
         synchronized(this) {
             val existingBasket = basketMap[request.sessionId]
-
             if (existingBasket.isNullOrEmpty()) basketMap[request.sessionId] = mutableMapOf(productCode to request.count)
             else {
                 val newCount = existingBasket[productCode]?.plus(request.count) ?: request.count
                 existingBasket[productCode] = newCount
                 basketMap[request.sessionId] = existingBasket
             }
+
+            return getBasket(request.sessionId)
         }
     }
-    fun removeItemFromBasket(request: BasketUpdateRequest) {
+    fun removeItemFromBasket(request: BasketUpdateRequest): List<Item> {
         val productCode = request.product.productCode
 
         synchronized(this) {
-            val existingBasket = basketMap[request.sessionId] ?: return
-            val productCountInBasket = existingBasket[productCode] ?: return
+            val existingBasket = basketMap[request.sessionId] ?: return getBasket(request.sessionId)
+            val productCountInBasket = existingBasket[productCode] ?: return getBasket(request.sessionId)
 
             if ((productCountInBasket - request.count) <= 0) {
                 existingBasket.remove(productCode)
@@ -35,15 +36,25 @@ class BasketService  {
                 existingBasket[productCode] = productCountInBasket - request.count
                 basketMap[request.sessionId] = existingBasket
             }
+
+            return getBasket(request.sessionId)
         }
     }
-
+    private fun getBasket(sessionId: Long) = basketMap[sessionId]
+        ?.entries
+        ?.map { Item(it.key, it.value) }
+        ?.toList() ?: emptyList()
 }
 
 data class Product(
     val productCode: Int,
     val productName: String,
     val price: Double,
+)
+
+data class Item(
+    val productCode: Int,
+    val count: Int,
 )
 
 data class BasketUpdateRequest(
--- src/test/kotlin/com/example/shoppingbasket/services/BasketServiceTest.kt
+++ src/test/kotlin/com/example/shoppingbasket/services/BasketServiceTest.kt
@@ -11,68 +11,87 @@ class BasketServiceTest {
 
     @Test
     fun addOneItem() {
-        sut.addItemToBasket(BasketUpdateRequest(1, productMilk, 2))
+        val actual = sut.addItemToBasket(BasketUpdateRequest(1, productMilk, 2))
+
         assertEquals(sut.basketMap.toString(), mutableMapOf(1 to mutableMapOf(11 to  2)).toString())
+        assertEquals(actual, listOf(Item(11,2)))
     }
 
     @Test
     fun addTheSameItemMultipleTimes() {
         sut.addItemToBasket(BasketUpdateRequest(1, productMilk, 2))
-        sut.addItemToBasket(BasketUpdateRequest(1, productMilk, 3))
+        val actual = sut.addItemToBasket(BasketUpdateRequest(1, productMilk, 3))
+
         assertEquals(sut.basketMap.toString(), mutableMapOf(1 to mutableMapOf(11 to  5)).toString())
+        assertEquals(actual, listOf(Item(11,5)))
     }
 
     @Test
     fun addMultipleItems() {
         sut.addItemToBasket(BasketUpdateRequest(1, productMilk, 3))
-        sut.addItemToBasket(BasketUpdateRequest(1, productEgg, 1))
+        val actual = sut.addItemToBasket(BasketUpdateRequest(1, productEgg, 1))
+
         assertEquals(sut.basketMap.toString(), mutableMapOf(1 to mutableMapOf(11 to 3, 22 to  1)).toString())
+        assertEquals(actual, listOf(Item(11,3), Item(22,1)))
     }
 
     @Test
     fun addToMultipleSessions() {
         sut.addItemToBasket(BasketUpdateRequest(1, productMilk, 3))
-        sut.addItemToBasket(BasketUpdateRequest(1, productMilk, 1))
+        val actual1 = sut.addItemToBasket(BasketUpdateRequest(1, productMilk, 1))
         sut.addItemToBasket(BasketUpdateRequest(2, productEgg, 1))
-        sut.addItemToBasket(BasketUpdateRequest(2, productMilk, 1))
+        val actual2 = sut.addItemToBasket(BasketUpdateRequest(2, productMilk, 1))
+
         assertEquals(sut.basketMap.toString(), mutableMapOf(1 to mutableMapOf(11 to 4),
             2 to mutableMapOf(22 to 1, 11 to 1)).toString())
+        assertEquals(actual1, listOf(Item(11,4)))
+        assertEquals(actual2, listOf(Item(22,1), Item(11,1)))
     }
 
     @Test
     fun removeOneItemUpdatesCount() {
         sut.addItemToBasket(BasketUpdateRequest(1, productMilk, 3))
-        sut.removeItemFromBasket(BasketUpdateRequest(1, productMilk, 1))
+        val actual = sut.removeItemFromBasket(BasketUpdateRequest(1, productMilk, 1))
+
         assertEquals(sut.basketMap.toString(), mutableMapOf(1 to mutableMapOf(11 to 2)).toString())
+        assertEquals(actual, listOf(Item(11,2)))
     }
 
     @Test
     fun removeOneItemEntirely() {
         sut.addItemToBasket(BasketUpdateRequest(1, productMilk, 3))
         sut.addItemToBasket(BasketUpdateRequest(1, productEgg, 1))
-        sut.removeItemFromBasket(BasketUpdateRequest(1, productMilk, 3))
+        val actual = sut.removeItemFromBasket(BasketUpdateRequest(1, productMilk, 3))
+
         assertEquals(sut.basketMap.toString(), mutableMapOf(1 to mutableMapOf(22 to 1)).toString())
+        assertEquals(actual, listOf(Item(22,1)))
     }
 
     @Test
     fun removeLastItemRemovesBasket() {
         sut.addItemToBasket(BasketUpdateRequest(1, productMilk, 3))
-        sut.removeItemFromBasket(BasketUpdateRequest(1, productMilk, 3))
+        val actual =  sut.removeItemFromBasket(BasketUpdateRequest(1, productMilk, 3))
+
         assertTrue(sut.basketMap.isEmpty())
+        assertEquals(actual, emptyList<Item>())
     }
 
     @Test
     fun removeItemNotInBasket() {
         sut.addItemToBasket(BasketUpdateRequest(1, productMilk, 3))
-        sut.removeItemFromBasket(BasketUpdateRequest(1, productEgg, 3))
+        val actual = sut.removeItemFromBasket(BasketUpdateRequest(1, productEgg, 3))
+
         assertEquals(sut.basketMap.toString(), mutableMapOf(1 to mutableMapOf(11 to 3)).toString())
+        assertEquals(actual, listOf(Item(11,3)))
     }
 
     @Test
     fun removeItemFromNonExistentBasket() {
         sut.addItemToBasket(BasketUpdateRequest(1, productMilk, 3))
-        sut.removeItemFromBasket(BasketUpdateRequest(2, productEgg, 3))
+        val actual = sut.removeItemFromBasket(BasketUpdateRequest(2, productEgg, 3))
+
         assertEquals(sut.basketMap.toString(), mutableMapOf(1 to mutableMapOf(11 to 3)).toString())
+        assertEquals(actual, emptyList<Item>())
     }
 
 }
